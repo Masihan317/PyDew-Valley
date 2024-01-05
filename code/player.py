23dea8559c996f251e2e9c52942cc1e36329b1f1
sihanm2 <sihanm2@uw.edu>
2024-01-05 14:36:36 -0800

    3:22:17

--- code/player.py
+++ code/player.py
@@ -4,7 +4,7 @@ from support import *
 from timer import Timer
 
 class Player(pygame.sprite.Sprite):
-  def __init__(self, pos, group, collision_sprites) -> None:
+  def __init__(self, pos, group, collision_sprites, tree_sprites) -> None:
     super().__init__(group)
 
     self.import_assets()
@@ -23,22 +23,49 @@ class Player(pygame.sprite.Sprite):
     self.hitbox = self.rect.copy().inflate((-126, -70))
     self.collision_sprites = collision_sprites
 
+    # timers
     self.timers = {
       "tool use": Timer(350, self.use_tool),
       "tool switch": Timer(200),
       "seed use": Timer(350, self.use_seed),
       "seed switch": Timer(200),
     }
+
+    # tools
     self.tools = ["hoe", "axe", "water"]
     self.tool_index = 0
     self.selected_tool = self.tools[self.tool_index]
 
+    # seeds
     self.seeds = ["corn", "tomato"]
     self.seed_index = 0
     self.selected_seed = self.seeds[self.seed_index]
 
+    # inventory
+    self.item_inventory = {
+      "wood": 0,
+      "apple": 0,
+      "corn": 0,
+      "tomato": 0
+    }
+
+    # interaction
+    self.tree_sprites = tree_sprites
+
   def use_tool(self):
-    pass
+    if self.selected_tool == "hoe":
+      pass
+
+    if self.selected_tool == "axe":
+      for tree in self.tree_sprites.sprites():
+        if tree.rect.collidepoint(self.target_pos):
+          tree.damage()
+
+    if self.selected_tool == "water":
+      pass
+
+  def get_target_pos(self):
+    self.target_pos = self.rect.center + PLAYER_TOOL_OFFSET[self.status.split("_")[0]]
 
   def use_seed(self):
     pass
@@ -162,5 +189,7 @@ class Player(pygame.sprite.Sprite):
     self.input()
     self.get_status()
     self.update_timers()
+    self.get_target_pos()
+
     self.move(dt)
     self.animate(dt)
--- code/sprites.py
+++ code/sprites.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import *
-from random import randint
+from random import randint, choice
+from settings import LAYERS
+from timer import Timer
 
 class Generic(pygame.sprite.Sprite):
   def __init__(self, pos, surf, groups, z = LAYERS["main"]) -> None:
@@ -38,15 +40,83 @@ class WildFlower(Generic):
     super().__init__(pos, surf, groups)
     self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
 
+class Particle(Generic):
+  def __init__(self, pos, surf, groups, z, duration = 200) -> None:
+    super().__init__(pos, surf, groups, z)
+    self.start_time = pygame.time.get_ticks()
+    self.duration = duration
+
+    # white surface
+    mask_surf = pygame.mask.from_surface(self.image)
+    new_surf = mask_surf.to_surface()
+    new_surf.set_colorkey((0, 0, 0))
+    self.image = new_surf
+
+  def update(self, dt):
+    current_time = pygame.time.get_ticks()
+    if current_time - self.start_time > self.duration:
+      self.kill()
+
 class Tree(Generic):
-  def __init__(self, pos, surf, groups, name) -> None:
+  def __init__(self, pos, surf, groups, name, all_sprites, player_add) -> None:
     super().__init__(pos, surf, groups)
+
+    # tree attributes
+    self.health = 5
+    self.alive = True
+    stump_path = "../graphics/stumps/"
+    if name == "Small":
+      stump_path += "small.png"
+    else:
+      stump_path += "large.png"
+    self.stump_surf = pygame.image.load(stump_path).convert_alpha()
+    self.invul_timer = Timer(200)
 
     # apples
     self.apple_surf = pygame.image.load("../graphics/fruit/apple.png").convert_alpha()
     self.apple_pos = APPLE_POS[name]
     self.apple_sprites = pygame.sprite.Group()
+    self.all_sprites = all_sprites
     self.create_fruit()
+
+    self.player_add = player_add
+
+  def damage(self):
+
+    # damaging the tree
+    self.health -= 1
+
+    # remove an apple
+    if len(self.apple_sprites.sprites()) > 0:
+      random_apple = choice(self.apple_sprites.sprites())
+      Particle(
+        pos = random_apple.rect.topleft,
+        surf = random_apple.image,
+        groups = self.all_sprites,
+        z = LAYERS["fruit"])
+      self.player_add("apple")
+      random_apple.kill()
+
+  def check_death(self):
+    if self.health <= 0:
+      Particle(
+        pos = self.rect.topleft,
+        surf = self.image,
+        groups = self.all_sprites,
+        z = LAYERS["fruit"],
+        duration = 300
+      )
+
+      self.image = self.stump_surf
+      self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+      self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
+      self.alive = False
+
+      self.player_add("wood")
+
+  def update(self, dt):
+    if self.alive:
+      self.check_death()
 
   def create_fruit(self):
     for pos in self.apple_pos:
@@ -56,5 +126,5 @@ class Tree(Generic):
         Generic(
           pos = (x, y),
           surf = self.apple_surf,
-          groups = [self.apple_sprites, self.groups()[0]],
+          groups = [self.apple_sprites, self.all_sprites],
           z = LAYERS["fruit"])
